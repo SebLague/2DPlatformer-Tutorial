37022096963c14c37fa5dd863006646a106cf278
Sebastian Lague <seblague@yahoo.com>
2015-04-03 21:54:59 +0200

    Episode One

---
+++ Episode 01/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController2D))]
+public class Player : MonoBehaviour {
+
+	float gravity = -22;
+
+	Vector3 velocity;
+
+	CharacterController2D controller;
+
+
+	void Start() {
+		controller = GetComponent<CharacterController2D> ();
+	}
+
+	void Update () {
+	
+
+
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
+
+	}
+}
